8b2fa51a0b7c445df56a4743b62f94d0a0000143
ErikWDev <erikwdev@gmail.com>
2021-12-24 10:19:33 +0100

    Not finished

--- solutions/2021/day_24.nim
+++ solutions/2021/day_24.nim
@@ -46,63 +46,107 @@ const
   zero = initBigInt(0)
   one = initBigInt(1)
 
-  C1 = [+12, +11, +12, -03, +10, -09, +10, -07, -11, -04, +14, +11, -08, -10].mapIt(initBigInt(it))
-  C2 = [+07, +15, +02, +15, +14, +02, +15, +01, +15, +15, +12, +02, +13, +13].mapIt(initBigInt(it))
-  C3 = [+01, +01, +01, +26, +01, +26, +01, +26, +26, +26, +01, +01, +26, +26].mapIt(initBigInt(it))
+  C1 = [+01, +01, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].mapIt(initBigInt(it))
+  C2 = [+15, +15, 02, 15, 14, 2, 15, 1, 15, 15, 12, 02, 13, 13].mapIt(initBigInt(it))
+
+  C3 = [1, 01, 1, 26, 1, 26, 1, 26, 26, 26, 01, 1, 26, 26].mapIt(initBigInt(it))
 
   twentySix = initBigInt(26)
+  twentyFive = initBigInt(25)
 
 
-proc runProgram(program: seq[Instruction], inputs: array[0..13, int]): BigInt =
+type
+  Key = (BigInt, BigInt)
+  Value = (BigInt, BigInt)
+  Cache = TableRef[Key, Value]
+
+
+proc runProgram(inputs: array[0..13, int], cache: Cache): BigInt =
   var
+    w = zero
     x = zero
     y = zero
     z = zero
-    w = zero
 
-  for i in 0 .. high(C1):
-    let (c1, c2, c3) = (C1[i], C2[i], C3[i])
+  func addOp(a: var BigInt, b: BigInt) = a = a + b
+  func mulOp(a: var BigInt, b: BigInt) = a = a * b
+  func modOp(a: var BigInt, b: BigInt) = a = a mod b
+  func divOp(a: var BigInt, b: BigInt) = a = a div b
+  func eqlOp(a: var BigInt, b: BigInt) = a = if a == b: one else: zero
 
-    w = initBigInt(inputs[i])
+  proc once(i: int) =
+    w = initBigInt(inputs[i]) # inpOp w
 
-    x = z mod twentySix
-    z = z div c3
-    x = if x + c1 == w: zero else: one
+    let key = (w, z)
+    if key in cache:
+      (w, z) = cache[key]
+      return
 
     let
-      a = if x == one: twentySix else: one
-      b = if x == one: w + c2 else: zero
+      c1 = C1[i]
+      c2 = C2[i]
+      c3 = C3[i]
 
-    z = z * a + b
+    mulOp x, zero
+    addOp x, z
+    modOp x, twentySix
+
+    divOp z, c1
+    addOp x, c2
+
+    eqlOp x, w
+    eqlOp x, zero
+    mulOp y, zero
+    addOp y, twentyFive
+    mulOp y, x
+    addOp y, one
+    mulOp z, y
+    mulOp y, zero
+    addOp y, w
+
+    addOp y, c3
+
+    mulOp y, x
+    addOp z, y
+
+    cache[key] = (w, z)
+
+  for i in 0 .. 13:
+    once(i)
 
   return z
 
 
 proc part1*(input: string): string =
-  let data = getData(input)
+  # let data = getData(input)
 
   var
-    current = [9, 9, 9, 9, 9, 9, 3, 1, 6, 6, 4, 7, 7, 8]
+    current = [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
     n = 0
-    biggest: BigInt = zero
+    biggest: array[0..13, int]
+    cache = new Cache
 
   while true:
     inc n
 
-    let value = data.runProgram(current)
+    let value = runProgram(current, cache)
 
-    if n mod 40000 == 0:
+    if n mod 800000 == 0:
       echo current, ": ", value
       n = 0
 
     if value == zero:
       echo "YES"
-      biggest = max(biggest, initBigInt(current.join("")))
-      echo biggest
+      if initBigInt(current.join("")) > initBigInt(biggest.join("")):
+        biggest = current
+
+      echo current.join("")
+      echo biggest.join("")
 
     current = nextDown(current)
 
-  return $biggest
+
+  return biggest.join("")
 
 proc part2*(input: string): auto =
   let data = getData(input)
