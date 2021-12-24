d0e366d894eb0fb80dfb89821da1725b30867d3e
ErikWDev <ErikWDev@gmail.com>
2021-12-24 21:37:21 +0100

    Unfinished 24:th

--- solutions/2021/day_24.nim
+++ solutions/2021/day_24.nim
@@ -45,55 +45,41 @@ func nextUp(values: array[0..13, int]): array[0..13, int] =
 const
   zero = initBigInt(0)
   one = initBigInt(1)
-
-  C1 = [+01, +01, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].mapIt(initBigInt(it))
-  C2 = [+15, +15, 02, 15, 14, 2, 15, 1, 15, 15, 12, 02, 13, 13].mapIt(initBigInt(it))
-
-  C3 = [1, 01, 1, 26, 1, 26, 1, 26, 26, 26, 01, 1, 26, 26].mapIt(initBigInt(it))
-
   twentySix = initBigInt(26)
   twentyFive = initBigInt(25)
 
 
 type
-  Key = (BigInt, BigInt)
-  Value = (BigInt, BigInt)
+  Key = (int, BigInt, BigInt)
+  Value = (BigInt, BigInt, BigInt, BigInt)
   Cache = TableRef[Key, Value]
 
 
-proc runProgram(inputs: array[0..13, int], cache: Cache): BigInt =
-  var
-    w = zero
-    x = zero
-    y = zero
-    z = zero
-
+proc runProgram(inputs: array[0..13, int], C1, C2, C3: seq[BigInt]): BigInt =
   func addOp(a: var BigInt, b: BigInt) = a = a + b
   func mulOp(a: var BigInt, b: BigInt) = a = a * b
   func modOp(a: var BigInt, b: BigInt) = a = a mod b
   func divOp(a: var BigInt, b: BigInt) = a = a div b
   func eqlOp(a: var BigInt, b: BigInt) = a = if a == b: one else: zero
 
+  var
+    w = zero
+    x = zero
+    y = zero
+    z = zero
+
   proc once(i: int) =
-    w = initBigInt(inputs[i]) # inpOp w
-
-    let key = (w, z)
-    if key in cache:
-      (w, z) = cache[key]
-      return
-
     let
       c1 = C1[i]
       c2 = C2[i]
       c3 = C3[i]
 
+    w = initBigInt(inputs[i]) # inpOp w
     mulOp x, zero
     addOp x, z
     modOp x, twentySix
-
     divOp z, c1
     addOp x, c2
-
     eqlOp x, w
     eqlOp x, zero
     mulOp y, zero
@@ -103,36 +89,49 @@ proc runProgram(inputs: array[0..13, int], cache: Cache): BigInt =
     mulOp z, y
     mulOp y, zero
     addOp y, w
-
     addOp y, c3
-
     mulOp y, x
     addOp z, y
-
-    cache[key] = (w, z)
 
   for i in 0 .. 13:
     once(i)
 
   return z
 
+import random
 
 proc part1*(input: string): string =
   # let data = getData(input)
 
+  let
+    theSplit = input.strip().split("inp w")[1 .. ^1]
+    C1 = theSplit.mapIt(it.splitLines()[04].scanTuple("div z $i")[1].initBigInt()).toSeq()
+    C2 = theSplit.mapIt(it.splitLines()[05].scanTuple("add $w $i")[2].initBigInt()).toSeq()
+    C3 = theSplit.mapIt(it.splitLines()[15].scanTuple("add $w $i")[2].initBigInt()).toSeq()
+
+  echo C1
+  echo C2
+  echo C3
+
+  const r = 1..9
+
   var
-    current = [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
+    # current = [8,9,9,5,9,7,9,9,9,9,9,9,9,9]
+    current = [8,9,9,9,9,9,9,9,9,9,9,9,9,9]
+    # Two hours on my laptop got me to: [8, 9, 9, 9, 7, 9, 2, 6, 4, 5, 6, 7, 1, 4]
+    # 98999987787498
+    # 99999987787587
     n = 0
     biggest: array[0..13, int]
-    cache = new Cache
 
   while true:
     inc n
 
-    let value = runProgram(current, cache)
+    let value = runProgram(current, C1, C2, C3)
 
-    if n mod 800000 == 0:
+    if n mod 40000 == 0:
       echo current, ": ", value
+      echo biggest.join("")
       n = 0
 
     if value == zero:
@@ -148,6 +147,7 @@ proc part1*(input: string): string =
 
   return biggest.join("")
 
+
 proc part2*(input: string): auto =
   let data = getData(input)
   return ""
@@ -161,12 +161,12 @@ proc main*() =
   if not runExamples1(date, part1): return
   let answer1 = part1(input)
   printAnswer(answer1)
-  submit1(date, answer1)
+  # submit1(date, answer1)
 
   if not runExamples2(date, part2): return
   let answer2 = part2(input)
   printAnswer(answer2)
-  submit2(date, answer2)
+  # submit2(date, answer2)
 
 
 when isMainModule:
